fdf7f12d95b09c826242e63299b1599a52bc315d
tarmes <traarmes@gmail.com>
2020-10-05 18:51:56 -0400

    basic apps built

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { initialState } from '../reducers'
+
+export default function TodoForm() {
+    // const[newTask, setNewTask] = useState(initialState)
+    // const[taskList, setTaskList] = useState(initialValues)
+
+
+    return(
+        null
+    )
+}
---
+++ todo/src/components/TodoList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TodoList() {
+    return (
+        <div className='todo-list-container'>
+            <h1>todolist state will go here</h1>
+        </div>
+    )
+}
