0e629d4f2e151f07dfb46502c8ccb89b4458ed95
tarmes <traarmes@gmail.com>
2020-10-05 18:03:38 -0400

    reducer and initialState

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,19 @@
+export const initialState = [
+    {
+        item: 'Learn about Reducers',
+        completed: false,
+        id: 3892987589
+    },
+    {
+        item: 'start the project',
+        completed: false,
+        id: 9874654132
+    }
+]
+
+export const todoReducer = (state, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
